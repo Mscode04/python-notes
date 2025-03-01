1e6910fa71e4a098c612f2f40c587ad2e9f2b3d5
Mscode04 <ms.shaheenkp@gmail.com>
2025-03-01 11:54:39 +0530

    app-updated2.01

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBohlRZge6RBys1BhSlH1zGLLRa6tjCjIU",
-  authDomain: "medical-504a6.firebaseapp.com",
-  projectId: "medical-504a6",
-  storageBucket: "medical-504a6.firebasestorage.app",
-  messagingSenderId: "625514501361",
-  appId: "1:625514501361:web:5844769cad2e22dd0a0550",
-  measurementId: "G-M1V48N38KM"
+  apiKey: "AIzaSyAPlZE4Nf9tCs04P-WiFCvZemSxJ0ivBms",
+  authDomain: "home-c8829.firebaseapp.com",
+  projectId: "home-c8829",
+  storageBucket: "home-c8829.firebasestorage.app",
+  messagingSenderId: "1009118507257",
+  appId: "1:1009118507257:web:86466c474988e9c3dfb228",
+  measurementId: "G-JC7MGP56DM"
 };
 
 
